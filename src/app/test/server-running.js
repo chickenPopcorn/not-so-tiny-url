05b1d7f09eca96a31b9d6d8dcb54fef852a7724b
Jiaxin Su <js4722@columbia.edu>
2016-11-07 23:29:49 -0500

    testing for Feed and URL

--- src/app/test/server-running.js
+++ src/app/test/server-running.js
@@ -84,52 +84,73 @@ describe('User', function() {
 describe('Feed', function() {
     var url = 'http://localhost:3000';
 
-    it('should return the most recent 2 public feed posts', function(done) {
-        request(url)
-            .get('/feed/public/2/-1')
-            .send()
-            .end(function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.should.have.property('status', 200);
-                done();
-            });
+    it('should return the most recent 2 public feed posts', function (done) {
+        userUrlService.getFeed(2, -1, true, -1, function (json) {
+            assert.equal(json.data.length, 2);
+            done();
+        });
     });
-    it('should return the metadata for www.google.com', function(done) {
-        request(url)
-            .get('/feed/meta/www.google.com')
-            .send()
-            .end(function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.should.have.property('status', 200);
-                done();
-            });
+    it('should return the metadata for www.google.com', function (done) {
+        userUrlService.getMeta("http://www.google.com", function (json) {
+            assert.equal(json.result.status, 'ok');
+            done();
+        });
     });
-    it('should return status 403 when trying to get the private feed without login', function(done) {
-        request(url)
-            .get('/feed/private/10/-1')
-            .send()
-            .end(function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.should.have.property('status', 403);
-                done();
-            });
+    it('should return status ok when getting the private feed for a user', function (done) {
+        userUrlService.getFeed(10, -1, false, '5801670fec83023b1c9e81c1', function (json) {
+            assert.equal(json.status, 'ok');
+            done();
+        });
     });
-    it('should return the number of likes for a certain post', function(done) {
-        request(url)
-            .get('/feed/post/likes/58030715c387a023e0fb1cb1')
-            .send()
-            .end(function(err, res) {
-                if (err) {
-                    throw err;
-                }
-                res.should.have.property('status', 200);
-                done();
+    it('should return the number of likes for a certain post', function (done) {
+        userUrlService.getNumberOfLikes('58030715c387a023e0fb1cb1', function (json) {
+            assert.equal(json.status, 'ok');
+            done();
+        });
+    });
+    it('should successfully like a post', function (done) {
+        userUrlService.getNumberOfLikes('58030715c387a023e0fb1cb1', function (json) {
+            var count = json.data.count;
+            userUrlService.like('58030715c387a023e0fb1cb1', '58214149f193d82e0032087b', 'For Test', function () {
+                userUrlService.getNumberOfLikes('58030715c387a023e0fb1cb1', function (json1) {
+                    var count1 = json1.data.count;
+                    // console.log('count:' + count);
+                    // console.log('count1:' + count1);
+                    assert.equal(count1, count + 1);
+                    done();
+                });
             });
+        });
+    });
+    it('should successfully unlike a post', function (done) {
+        userUrlService.getNumberOfLikes('58030715c387a023e0fb1cb1', function (json) {
+            var count = json.data.count;
+            userUrlService.unlike('58030715c387a023e0fb1cb1', '58214149f193d82e0032087b', function () {
+                userUrlService.getNumberOfLikes('58030715c387a023e0fb1cb1', function (json1) {
+                    var count1 = json1.data.count;
+                    // console.log('count:' + count);
+                    // console.log('count1:' + count1);
+                    assert.equal(count1, count - 1);
+                    done();
+                });
+            });
+        });
+    });
+});
+
+describe('URL', function() {
+    var url = 'http://localhost:3000';
+
+    it('should return a short URL for google.com', function(done) {
+        urlService.getShortUrl("http://www.google.com", function(json) {
+            assert.equal(json.status, 'ok');
+            done();
+        });
+    });
+    it('should return the long URL given a short URL', function(done) {
+        urlService.getLongUrl("a", function(json) {
+            assert.equal(json.status, 'ok');
+            done();
+        });
     });
 });
