8da88f37428d3d5ddb5a8b9401f4d4891a399f88
Tianci Zhong <tz2278@columbia.edu>
2016-12-02 20:29:32 -0500

    change code styles for public/js

--- src/app/public/js/services/rankService.js
+++ src/app/public/js/services/rankService.js
@@ -1,17 +1,16 @@
-angular.module('tinyurlApp')
-    .factory('rankService', function($http) {
-        return {
-            saveUrlClicks: function() {
-                return $http.get('/rank/saveUrlClicks/');
-            },
-            getAllClicks: function() {
-                return $http.get('/rank/getAllClicks/');
-            },
-            getTopKUrls: function(k) {
-                return $http.get('/rank/getTopKUrls/' + k);
-            },
-            getUrlClicks: function(shortUrl) {
-                return $http.get('/rank/getClicks/' + shortUrl);
-            }
-        };
-    });
+angular.module('tinyurlApp').factory('rankService', function($http) {
+    return {
+        saveUrlClicks: function() {
+            return $http.get('/rank/saveUrlClicks/');
+        },
+        getAllClicks: function() {
+            return $http.get('/rank/getAllClicks/');
+        },
+        getTopKUrls: function(k) {
+            return $http.get('/rank/getTopKUrls/' + k);
+        },
+        getUrlClicks: function(shortUrl) {
+            return $http.get('/rank/getClicks/' + shortUrl);
+        }
+    };
+});
