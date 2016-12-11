c90f0679f3b68e59f757b413a1c42dc31d0a5b88
Ruicong Xie <rxie25@gmail.com>
2016-12-11 02:07:39 -0500

    using request-ip instead of manually parse

--- src/app/services/statsService.js
+++ src/app/services/statsService.js
@@ -1,6 +1,7 @@
 var geoip = require('geoip-lite');
 var RequestModel = require('../models/requestModel');
 var rankUrlService = require('../services/rankUrlService');
+var requestIp = require('request-ip');
 
 var logRequest = function(shortUrl, req) {
     var reqInfo = {};
@@ -8,10 +9,8 @@ var logRequest = function(shortUrl, req) {
     reqInfo.referer = req.headers.referer || 'Unknown';
     reqInfo.platform = req.useragent.platform || 'Unknown';
     reqInfo.browser = req.useragent.browser || 'Unknown';
-    var ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
+    var ip = requestIp.getClientIp(req);
+    console.log("ip is"+ ip);
     var geo = geoip.lookup(ip);
     if (geo) {
         reqInfo.country = geo.country;
