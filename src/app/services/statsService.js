02ec11b0274b1b7d06abd775470520581afb415d
Tianci Zhong <tz2278@columbia.edu>
2016-12-12 16:15:46 -0500

    fix requestmodel add data and clean up database

--- src/app/services/statsService.js
+++ src/app/services/statsService.js
@@ -1,7 +1,15 @@
 var geoip = require('geoip-lite');
 var RequestModel = require('../models/requestModel');
+var UrlModel = require('../models/urlModel');
 var rankUrlService = require('../services/rankUrlService');
 var requestIp = require('request-ip');
+var redis = require('redis');
+
+var port = process.env.REDIS_PORT_6379_TCP_PORT;
+var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+
+var redisClient = redis.createClient(port, host);
+var hashClick = '_clicks';
 
 var logRequest = function(shortUrl, req) {
     var reqInfo = {};
@@ -17,22 +25,55 @@ var logRequest = function(shortUrl, req) {
         reqInfo.country = 'Unknown';
     }
     reqInfo.timestamp = new Date();
-    var request = new RequestModel(reqInfo);
-    request.save(function(err, data) {
-        if (err != null) {
-            console.log(err);
-            return;
-        }
-        // console.log('request saved: ' + data);
-        if (data.shortUrl != 'undefined' && data.shortUrl != 'favicon.ico') {
-            rankUrlService.updateUrlClicks(data.shortUrl, function(err, data) {
-                if (err != null) {
-                    console.log(err);
-                }
-            });
+
+    redisClient.keys('*', function(err, data) {
+        console.log(data);
+        if (data.length > 0) {
+            if (data.indexOf(shortUrl) > -1) {
+                var request = new RequestModel(reqInfo);
+                request.save(function(err, data) {
+                    if (err != null) {
+                        console.log(err);
+                        return;
+                    }
+                    // console.log('request saved: ' + data);
+                    if (data.shortUrl != 'undefined'
+                        && data.shortUrl != 'favicon.ico') {
+                        rankUrlService.updateUrlClicks(data.shortUrl,
+                            function(url, data) {
+                                if (url != shortUrl) {
+                                    console.log(url);
+                                }
+                        });
+                    }
+                });
+            }
+        } else {
+            // var reg = /[abc]*\/[abc]*/i;
+            // RequestModel.remove({shortUrl: reg}, function(err, data) {
+            //     console.log(data);
+            // });
+            UrlModel.find({shortUrl: shortUrl}, function(err, data) {
+                var request = new RequestModel(reqInfo);
+                request.save(function(err, data) {
+                    if (err != null) {
+                        console.log(err);
+                        return;
+                    }
+                    // console.log('request saved: ' + data);
+                    if (data.shortUrl != 'undefined'
+                        && data.shortUrl != 'favicon.ico') {
+                        rankUrlService.updateUrlClicks(data.shortUrl,
+                            function(url, data) {
+                                if (url != shortUrl) {
+                                    console.log(url);
+                                }
+                            });
+                    }
+                });
+            })
         }
     });
-
 };
 
 var getUrlInfo = function(shortUrl, info, callback) {
